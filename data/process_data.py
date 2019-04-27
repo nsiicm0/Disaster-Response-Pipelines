33b606ddf12969a856e91058195c2c71f280bdce
Niclas Simmler <niclas.simmler@ch.pwc.com>
2019-04-27 22:44:36 +0200

    Implemented ETL pipeline for flat file data.
    Loads the data from the CSVs provided upon execution and processes the data.
    The data will then be stored in a sqlite db.

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,76 @@
 import sys
-
+import pandas as pd
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    """Loads the data specified by the paths in the arguments and merges it to one dataframe.
+
+    Args:
+        messages_filepath: The file path to the messages csv. Must contain an 'id' column.
+        categories_filepath: The file path to the categories csv. Must contain an 'id' column.
+
+    Returns:
+        Combined dataframe holding both files.
+
+    """
+    # Load files
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+
+    # Merge files
+    df = pd.merge(messages, categories, on='id', how='inner')
+
+    return df
 
 
 def clean_data(df):
-    pass
+    """Cleans the dataframe by expanding columns which were initially concatenated. Also removes duplicate rows.
+
+    Args:
+        df: The dataframe to be cleaned.
+
+    Returns:
+        Cleaned dataframe
+
+    """
+    # Expand concatenated categories into new dataframe
+    categories = df.categories.str.split(';', expand=True)
+
+    # Extract column names and set columns in new dataframe
+    row = categories.head(1)
+    category_colnames = list(map(lambda x: x.split('-')[0], row.values.tolist()[0]))
+    categories.columns = category_colnames
+
+    # Convert column values to actual integer values (before: colA-1 --> after: 1)
+    for column in categories:
+        categories[column] = categories[column].map(lambda x: x.split('-')[1])
+        categories[column] = categories[column].map(lambda x: int(x))
+
+    # Replace existing categories column with expanded columns
+    df.drop(columns=["categories"], inplace = True)
+    df = pd.concat([df, categories], axis=1)
+
+    # Drop duplicates
+    df = df.drop_duplicates()
+
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    """Stores the dataframe as a sqlite database.
+
+    Args:
+        df: The dataframe to be stored.
+        database_filename: The database filename under which it will be stored. Must be in the format of "database.db".
+            Will also be used for the table name (without file extension)
+
+    Returns:
+        Nothing
+
+    """
+    # Store dataframe to sqlite db
+    engine = create_engine('sqlite:///{}'.format(database_filename))
+    df.to_sql('{}'.format(database_filename.replace('.db','')), engine, index=False)
 
 
 def main():
